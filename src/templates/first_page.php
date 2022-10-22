b9393fd4221b471c6a4241f3d5a90bf5dfd4d5a0
0980491 <86085545+0980491@users.noreply.github.com>
2022-10-22 16:16:06 -0500

    push

--- src/templates/first_page.php
+++ src/templates/first_page.php
@@ -2,7 +2,8 @@
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>CodePen - Log In / Sign Up - pure css - #12</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <title>Log In/title>
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css'>
   <link rel='stylesheet' href='https://unicons.iconscout.com/release/v2.1.9/css/unicons.css'><link rel="stylesheet">
   <link rel="stylesheet" href="{{ url_for('static', filename='css/style.css') }}"><link rel="stylesheet">
